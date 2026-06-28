a670be68512382e9325b9c6ed72282bf56f4f9ba
Mohammad <112726420+MohammadJamshidi1@users.noreply.github.com>
2026-06-28 18:06:31 +0330

    change directory

--- university/src/main/java/ir/chat/repository/ProfessorRepository.java
+++
@@ -1,7 +0,0 @@
-package ir.chat.repository;
-
-import ir.chat.entity.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessorRepository extends JpaRepository<Professor , Long> {
-}
